2eb2ad0328b535df9e7b09e7740027acad1d9ead
venikhl <g.pavlov2019@gmail.com>
2023-07-04 18:02:58 +0300

    7. All the basic functionality and design are ready.

--- entertainments/urls.py
+++ entertainments/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path('all/', views.entertainments_choice, name='choose-entertainments'),
-    path('all/movies', views.all_movies, name='all_movies')
+    path('all/movies', views.all_movies, name='all_movies'),
+    path('all/books', views.all_books, name='all_books'),
+    path('all/tvseries', views.all_tvseries, name='all_tvseries'),
+    path('all/movies/<int:movie_id>/', views.movie_detail, name='movie_detail'),
+    path('all/books/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('all/tvseries/<int:tvseries_id>/', views.tvseries_detail, name='tvseries_detail'),
+    path('all/movies/create_progress/', views.create_movie_progress, name='create_movie_progress'),
+    path('all/books/create_progress/', views.create_book_progress, name='create_book_progress'),
+    path('all/tvseries/create_progress/', views.create_tvseries_progress, name='create_tvseries_progress'),
+    path('all/mine/', views.all_my_progress, name='all_my_progress'),
 ]
--- entertainments/views.py
+++ entertainments/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Book, TVSeries, Movie
+from .models import Book, TVSeries, Movie, Progress
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
 
 def entertainments_choice(request):
@@ -9,9 +8,107 @@ def entertainments_choice(request):
 
 
 def all_movies(request):
-    movies_list = Book.objects.all()
+    movies_list = Movie.objects.all()
     return render(request, 'entertainments/movies.html',
                   {'movies_list': movies_list})
+
+
+def all_books(request):
+    books_list = Book.objects.all()
+    return render(request, 'entertainments/books.html',
+                  {'books_list': books_list})
+
+
+def all_tvseries(request):
+    tvseries_list = TVSeries.objects.all()
+    return render(request, 'entertainments/tvseries.html',
+                  {'tvseries_list': tvseries_list})
+
+
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'entertainments/movie_detail.html', {'movie': movie})
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'entertainments/book_detail.html', {'book': book})
+
+
+def tvseries_detail(request, tvseries_id):
+    tvseries = get_object_or_404(TVSeries, pk=tvseries_id)
+    return render(request, 'entertainments/tvseries_detail.html', {'tvseries': tvseries})
+
+def create_movie_progress(request):
+    if request.method == 'POST':
+        movie_id = request.POST.get('movie')
+        user = request.user
+        movie = Movie.objects.get(id=movie_id)
+        status = request.POST.get('status')
+        progress_type = request.POST.get('progress_type')
+
+        # Check if a progress object already exists for the movie and user
+        try:
+            progress = Progress.objects.get(user=user, progress_type=progress_type, movie=movie)
+            progress.status = status
+            progress.save()
+        except Progress.DoesNotExist:
+            progress = Progress.objects.create(user=user, movie=movie, progress_type=progress_type, status=status)
+            progress.save()
+
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error'})
+
+
+def create_book_progress(request):
+    if request.method == 'POST':
+        book_id = request.POST.get('book')
+        user = request.user
+        book = Book.objects.get(id=book_id)
+        status = request.POST.get('status')
+        progress_type = request.POST.get('progress_type')
+
+        # Check if a progress object already exists for the movie and user
+        try:
+            progress = Progress.objects.get(user=user, progress_type=progress_type, book=book)
+            progress.status = status
+            progress.save()
+        except Progress.DoesNotExist:
+            progress = Progress.objects.create(user=user, book=book, progress_type=progress_type, status=status)
+            progress.save()
+
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error'})
+
+
+def create_tvseries_progress(request):
+    if request.method == 'POST':
+        tvseries_id = request.POST.get('tvseries')
+        user = request.user
+        tvseries = TVSeries.objects.get(id=tvseries_id)
+        status = request.POST.get('status')
+        progress_type = request.POST.get('progress_type')
+
+        # Check if a progress object already exists for the movie and user
+        try:
+            progress = Progress.objects.get(user=user, progress_type=progress_type, tvseries=tvseries)
+            progress.status = status
+            progress.save()
+        except Progress.DoesNotExist:
+            progress = Progress.objects.create(user=user, tvseries=tvseries, progress_type=progress_type, status=status)
+            progress.save()
+
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error'})
+
+
+def all_my_progress(request):
+    progress_list = Progress.objects.all()
+    return render(request, 'entertainments/all_my_progress.html', {'progress_list': progress_list})
+
 
 # def all_entertainments(request):
 #     book_list = Book.objects.all()
